c6cc772daf8c8a6603b548d37b67bda96c79a0c4
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2023-08-25 21:49:18 -0400

    idk what i did

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import FadeIn from 'react-fade-in';
 import video from './Untitled_Artwork 2.MOV'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <FadeIn><h1>Hello there! I'm</h1></FadeIn>
+        
         <video autoPlay muted src={video} width="1000" height="500" />
       </header>
     </div>
