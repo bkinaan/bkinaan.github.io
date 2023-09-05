a5f48f5e17134e67bb1519bd88ecebe240e417c5
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2023-09-05 19:36:37 -0400

    started skills list

---
+++ portfolio/src/app/components/skills-list.tsx
@@ -0,0 +1,8 @@
+export default function Skills() {
+    return (
+        <div>
+            <div className='text-6xl text-center pb-10 font-ss3 font-black'>Relevant Skills</div>
+            
+        </div>
+    );
+}
