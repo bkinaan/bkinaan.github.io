5c0996cf39a9758f1ecb2a995262bdd537d206cc
bkinaan <89668762+bkinaan@users.noreply.github.com>
2023-09-05 22:49:55 -0400

    reformatted description of me

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -6,20 +6,22 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between">
             <Top />
-            <div className="text-white pt-24 pb-0 text-4xl font-ss3 font-semibold text-center z-10">Hello there! I'm</div>
+            <div className="text-black bg-white text-4xl p-2 mt-36 rounded-lg font-ss3 font-semibold text-center z-10">Hello there! I'm...</div>
             <div>
-                <video className="relative -mt-24 z-0" autoPlay muted>
+                <video className="z-0" autoPlay muted>
                     <source src="assets/name.MP4" type="video/mp4"/>
                 </video>
-                <div className='z-20 relative -mt-48 mb-36 text-white pb-10 text-2xl italic font-ss3 font-light text-center'>A Junior studying Computer Science with a minor in Psychology at Purdue University.</div>
             </div>
+            <div className='z-20 mb-36 py-5 px-2 text-4xl font-ss3 font-bold text-center text-black bg-white rounded-lg scale-125'>Junior | Honors | Computer Science @ Purdue Univeristy</div>
             <div className='z-30'>
                 <Projects />
             </div>
             <div className='mb-48'></div>
             <Skills />
             <a href='https://github.com/bkinaan/bkinaan.github.io'>
-            <div className='text-white mt-16 mb-8 p-2 border-2 rounded-md shadow-md shadow-slate-100 hover:translate-y-2 hover:shadow-sm hover:shadow-slate-100 hover:text-black hover:bg-white transition duration-300 ease-in-out'>Made By Brandon Kinaan | Code on GitHub</div></a>
+                <div className='text-white p-2 border-2 rounded-md shadow-md shadow-slate-100 hover:translate-y-2 hover:shadow-sm hover:shadow-slate-100 hover:text-black hover:bg-white transition duration-300 ease-in-out'>Made By Brandon Kinaan | Code on GitHub</div>
+            </a>
+            <div className='pt-10'></div>
         </main>
     );
 }
