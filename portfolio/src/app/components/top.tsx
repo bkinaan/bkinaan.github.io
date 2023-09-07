7cfc78ea78ed147eccead17f734cbdf520af8e26
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2023-09-07 13:21:28 -0400

    moved to top of screen so it doesn't get in way of brandon animation

--- portfolio/src/app/components/top.tsx
+++ portfolio/src/app/components/top.tsx
@@ -1,6 +1,6 @@
 export default function Top() {
     return(
-        <div className="fixed bottom-0 right-0 z-50 pr-5">
+        <div className="fixed top-0 right-0 z-50 pr-5 pt-5">
             <a href="https://drive.google.com/file/d/18Z9dqORrvedFBLMUAjV9arA9NQeeEA6n/view?usp=drive_link">
             {/* <button className="bg-white rounded-full w-20 mb-4 max-w-md shadow-md shadow-slate-100 hover:translate-y-2 hover:shadow-md hover:shadow-slate-100 transition duration-300 ease-in-out"> */}
             <button className="bg-white rounded-full w-20 mb-4 max-w-md shadow-md shadow-slate-100 hover:translate-y-1 hover:shadow-sm hover:shadow-slate-100 transition duration-300 ease-in-out">
