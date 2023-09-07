4707ef9a90dc9c03de7e58763f56e752c10e528f
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2023-09-07 13:24:15 -0400

    updated skill list to one file

--- portfolio/src/app/components/skill.tsx
+++
@@ -1,8 +0,0 @@
-export default function Skill() {
-    return (
-        <div className="border-4 border-white pl-3">
-            <div className="text-white text-ss3 text-5xl font-bold pt-3">Languages</div>
-            <div className="text-white text-ss3">Test</div>
-        </div>
-    );
-}
--- portfolio/src/app/components/skills-list.tsx
+++ portfolio/src/app/components/skills-list.tsx
@@ -1,10 +1,117 @@
-import Skill from "./skill";
-
 export default function Skills() {
+    const langauges = [
+        {
+            name: "Java"
+        },
+        {
+            name: "Python"
+        },
+        {
+            name: "C"
+        },
+        {
+            name: "C++"
+        },
+        {
+            name: "Javascript"
+        },
+        {
+            name:"Typescript"
+        },
+        {
+            name: "Bash"
+        },
+        {
+            name: "HTML"
+        },
+        {
+            name: "CSS"
+        },
+        {
+            name: "SQL"
+        },
+    ];
+
+    const framebraries = [
+        {
+            name: "React"
+        },
+        {
+            name: "Next.js"
+        },
+        {
+            name: "TailwindCSS"
+        },
+        {
+            name: "Swing"
+        },
+    ];
+
+    const tools = [
+        {
+            name: "Git"
+        },
+        {
+            name: "GitHub"
+        },
+        {
+            name: "VS Code"
+        },
+        {
+            name: "IntelliJ"
+        },
+        {
+            name: "Vim"
+        },
+        {
+            name: "GDB"
+        },
+        {
+            name: "Firebase"
+        },
+        {
+            name: "Flex"
+        },
+        {
+            name: "Bison"
+        },
+    ];
     return (
         <div className="mb-10">
-            <div className='text-6xl text-center pb-10 font-ss3 font-black'>Relevant Skills</div>
-            <Skill />
+            <div className='text-6xl text-center pb-10 font-ss3 font-black'>Technical Skills</div>
+            <div className="border-4 border-white pl-3 rounded-lg mb-10">
+                <div className="text-white text-ss3 text-5xl font-bold pt-3">Languages</div>
+                <div className="grid grid-cols-6 text-center gap-3 py-3 pr-3">
+                    {langauges.map((item, index) => (
+                        <div key={index}>
+                            <div className="text-white text-ss3 border-2 px-3 py-1 rounded-full">{item.name}</div>
+                        </ div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="border-4 border-white pl-3 rounded-lg mb-10">
+                <div className="text-white text-ss3 text-5xl font-bold pt-3">Frameworks/Libraries</div>
+                <div className="grid grid-cols-6 text-center gap-3 py-3 pr-3">
+                    {framebraries.map((item, index) => (
+                        <div key={index}>
+                            <div className="text-white text-ss3 border-2 px-3 py-1 rounded-full">{item.name}</div>
+                        </ div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="border-4 border-white pl-3 rounded-lg">
+                <div className="text-white text-ss3 text-5xl font-bold pt-3">Developer Tools</div>
+                <div className="grid grid-cols-6 text-center gap-3 py-3 pr-3">
+                    {tools.map((item, index) => (
+                        <div key={index}>
+                            <div className="text-white text-ss3 border-2 px-3 py-1 rounded-full">{item.name}</div>
+                        </ div>
+                    ))}
+                </div>
+            </div>
+
         </div>
     );
 }
