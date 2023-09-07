bbd57535f0c69e7b05276b4d2a206e93eecdff89
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2023-09-07 13:22:23 -0400

    formatting corners removed cause it wasn't working

--- portfolio/src/app/components/project.tsx
+++ portfolio/src/app/components/project.tsx
@@ -9,15 +9,16 @@ export default function Project(props: {name: string, lang: string, desc: string
         - bl => bottom left
         - br => bottom right
     */
-    let type = "";
+    /* let type = "";
     if (props.pos != "m"){
         type=`border-4 rounded-${props.pos}-lg max-w-md shadow-lg shadow-slate-100 hover:translate-y-2 hover:shadow-md hover:shadow-slate-100 transition duration-300 ease-in-out`
     } else {
         type="border-4 max-w-md shadow-lg shadow-slate-100 hover:translate-y-2 hover:shadow-md hover:shadow-slate-100 transition duration-300 ease-in-out"
-    }
+    } */
 
     return (
-    <div className={type}>
+    // <div className={type}>
+    <div className="border-4 max-w-md shadow-lg shadow-slate-100 hover:translate-y-2 hover:shadow-md hover:shadow-slate-100 transition duration-300 ease-in-out">
         <div className="pl-3 pb-5 pr-3">
             <a href={props.link}>
             <div className="text-5xl pt-3 font-ss3 font-bold">{props.name}</div>
