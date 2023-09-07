9a16a8c4e779767336ad3295bfff53d2589ff151
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2023-09-07 14:34:05 -0400

    cropped name animation for better formatting

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
         <main className="flex min-h-screen flex-col items-center justify-between">
             <Top />
             <div className="text-black bg-white text-4xl p-2 mt-36 rounded-lg font-ss3 font-semibold text-center z-10">Hello, there! I'm...</div>
-            <div>
+            <div className='pb-10'>
                 <video className="z-0" autoPlay muted>
-                    <source src="assets/name.MP4" type="video/mp4"/>
+                    <source src="assets/name_cropped.MP4" type="video/mp4"/>
                 </video>
             </div>
             <div className='z-20 mb-36 py-5 px-2 text-4xl font-ss3 font-bold text-center text-black bg-white rounded-lg'>Junior | Honors | Computer Science @ Purdue Univeristy</div>
@@ -23,9 +23,9 @@ export default function Home() {
             <Experiences />
             <Involvements />
             <a href='https://github.com/bkinaan/bkinaan.github.io'>
-                <div className='text-white p-2 border-2 rounded-md shadow-md shadow-slate-100 hover:translate-y-2 hover:shadow-sm hover:shadow-slate-100 hover:text-black hover:bg-white transition duration-300 ease-in-out'>Made by Brandon Kinaan | Code on GitHub</div>
+                <div className='text-white p-2 mt-5 border-2 rounded-md shadow-md shadow-slate-100 hover:translate-y-2 hover:shadow-sm hover:shadow-slate-100 hover:text-black hover:bg-white transition duration-300 ease-in-out'>Made by Brandon Kinaan | Code on GitHub</div>
             </a>
-            <div className='pt-10'></div>
+            <div className='pt-5'></div>
         </main>
     );
 }
