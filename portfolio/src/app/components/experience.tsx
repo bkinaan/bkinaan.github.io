324c725b7f5d3adfb849d6883a315ff1e872dd51
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2023-09-07 14:31:50 -0400

    added rounded corner formatting

--- portfolio/src/app/components/experience.tsx
+++ portfolio/src/app/components/experience.tsx
@@ -1,6 +1,30 @@
-export default function Experience(props: {role: string, company: string, time: string, description: string, link: string}) {
+export default function Experience(props: {role: string, company: string, time: string, description: string, link: string, pos: string}) {
+    /* Positioning of elements:
+        - tl => top left
+        - tr => top right
+        - m => middle
+        - bl => bottom left
+        - br => bottom right
+        - l => left
+        - r => right
+    */
+
+    let type = "";
+    if (props.pos != "m") {
+        type=`border-4 rounded-${props.pos}-lg max-w-md shadow-lg shadow-slate-100 hover:translate-y-2 hover:shadow-md hover:shadow-slate-100 transition duration-300 ease-in-out`
+    } else {
+        type="border-4 max-w-md shadow-lg shadow-slate-100 hover:translate-y-2 hover:shadow-md hover:shadow-slate-100 transition duration-300 ease-in-out"
+    }
+    
     return (
-        <div className="border-4 max-w-md shadow-lg shadow-slate-100 hover:translate-y-2 hover:shadow-md hover:shadow-slate-100 transition duration-300 ease-in-out">
+        // <div className="border-4 max-w-md shadow-lg shadow-slate-100 hover:translate-y-2 hover:shadow-md hover:shadow-slate-100 transition duration-300 ease-in-out">
+        <div className={type}>
+            <div className="opacity-0 rounded-tr-lg"></div>
+            <div className="opacity-0 rounded-tl-lg"></div>
+            <div className="opacity-0 rounded-br-lg"></div>
+            <div className="opacity-0 rounded-bl-lg"></div>
+            <div className="opacity-0 rounded-l-lg"></div>
+            <div className="opacity-0 rounded-r-lg"></div>
             <div className="pl-3 pb-5 pr-3">
                 <a href={props.link}>
                     <div className="text-white font-ss3 text-5xl font-bold pt-3">{props.role}</div>
--- portfolio/src/app/components/experiences-list.tsx
+++ portfolio/src/app/components/experiences-list.tsx
@@ -7,6 +7,7 @@ export default function Experiences() {
         time: "May 2021 - Aug. 2023",
         description: "I worked as a lead counselor in a variying number of roles. I led the media lab that included animation iPads and programming applications. I also led the robotics studio that included Sphero and Vex robotics. I was also tasked with running two daily all-camp activities that included 200+ campers where I managed other counselors.",
         link: "https://steveandkatescamp.com",
+        pos: "l"
     }
     let library = {
         role: "Library Page",
@@ -14,13 +15,14 @@ export default function Experiences() {
         time: "Oct. 2018 - May 2020",
         description: "I worked first as an intern and then as a library helper where I shelved books and ensuring library services ran efficently. I also operated opening and closing procedures to ensure that the library was ready for patrons.",
         link: "https://menlopark.gov/Government/Departments/Library-and-Community-Services/Library",
+        pos: "r"
     }
     return (
     <div className="mb-10 pt-36">
             <div className='text-6xl text-center pb-10 font-ss3 font-black'>Experience</div>
             <div className="grid grid-cols-2 gap-7">
-                <Experience role={sk.role} company={sk.company} time={sk.time} description={sk.description} link={sk.link}/>
-                <Experience role={library.role} company={library.company} time={library.time} description={library.description} link={library.link}/>
+                <Experience role={sk.role} company={sk.company} time={sk.time} description={sk.description} link={sk.link} pos={sk.pos}/>
+                <Experience role={library.role} company={library.company} time={library.time} description={library.description} link={library.link} pos={library.pos}/>
             </div>
     </div>
     );
