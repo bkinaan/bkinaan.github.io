25a71d7c52b4ab714d50b02f84ee148b0dc9c366
bkinaan <89668762+bkinaan@users.noreply.github.com>
2023-09-07 22:49:21 -0400

    one final padding/margin change for mobile for VERSION 1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         </Head>
             <main className="flex min-h-screen flex-col items-center justify-between bg-black">
                 <Top />
-                <div className="text-black bg-white text-2xl md:text-4xl p-2 mt-36 rounded-lg font-ss3 font-semibold text-center z-10">Hello, there! I&apos;m...</div>
+                <div className="text-black bg-white text-2xl md:text-4xl px-1 mb-4 md:p-2 mt-36 rounded-lg font-ss3 font-semibold text-center z-10">Hello, there! I&apos;m...</div>
                 <div className='pb-10'>
                     <video className="z-0" autoPlay muted playsInline>
                         <source src="assets/name_cropped.mp4" type="video/mp4"/>
