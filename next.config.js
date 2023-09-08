1be97d7ad1f691d6d73542b4f7c5db69bad90ff5
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2023-09-08 19:16:24 -0400

    doing some clean-up

--- next.config.js
+++ next.config.js
@@ -4,30 +4,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-/* const repo = 'bkinaan.github.io'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-} */
-
-/* const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-}
- */
