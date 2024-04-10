9b8a212fb22cef9c25b1af0628c5b96e9f1af1a5
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2024-04-09 22:13:03 -0400

    added saxbys to work experience

--- src/app/components/experiences-list.tsx
+++ src/app/components/experiences-list.tsx
@@ -1,6 +1,15 @@
 import Experience from "./experience";
 
 export default function Experiences() {
+  let saxbys = {
+    role: "Shift Lead",
+    company: "Saxbys Coffee",
+    time: "Feb. 2024 - current",
+    description:
+      "As a shift lead for Saxbys, I manage and run the cafe alongside our SCEO. I am in charge of leading a group of team members to create various beverage and food items up to Saxbys standards. I also monitor the cafe for prep and delegate tasks to team members to maintain readiness for guests. In addition to working at the cafe, I am also enrolled in Saxbys Experiential Learning Platform. I am working on a number of badge courses to expand my skills both in and out of the cafe.",
+    link: "https://saxbyscoffee.com",
+    pos: "l",
+  };
   let sk = {
     role: "Seasonal Lead Counselor",
     company: "Steve and Kate's Camp",
@@ -8,7 +17,7 @@ export default function Experiences() {
     description:
       "I worked as a lead counselor in a variying number of roles. I led the media lab that included animation iPads and programming applications. I also led the robotics studio that included Sphero and Vex robotics. I was also tasked with running two daily all-camp activities that included 200+ campers where I managed other counselors.",
     link: "https://steveandkatescamp.com",
-    pos: "l",
+    pos: "r",
   };
   let library = {
     role: "Library Page",
@@ -17,7 +26,7 @@ export default function Experiences() {
     description:
       "I worked first as an intern and then as a library helper where I shelved books and ensuring library services ran efficently. I also operated opening and closing procedures to ensure that the library was ready for patrons.",
     link: "https://menlopark.gov/Government/Departments/Library-and-Community-Services/Library",
-    pos: "r",
+    pos: "l",
   };
   return (
     <div className="mb-10 pt-36">
@@ -25,6 +34,14 @@ export default function Experiences() {
         Work Experience
       </div>
       <div className="grid grid-cols-2 gap-7">
+        <Experience
+          role={saxbys.role}
+          company={saxbys.company}
+          time={saxbys.time}
+          description={saxbys.description}
+          link={saxbys.link}
+          pos={saxbys.pos}
+        />
         <Experience
           role={sk.role}
           company={sk.company}
