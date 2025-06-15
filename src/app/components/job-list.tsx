042f18ce6d578c89401e734e71c4fde99d0898d3
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2025-06-14 17:39:07 -0700

    added current work experience

--- src/app/components/job-list.tsx
+++ src/app/components/job-list.tsx
@@ -5,13 +5,13 @@ export default function Jobs() {
     role: "IT Operations Consultant",
     company: "FDM Group",
     time: "Jun. 2025 - current",
-    desc: "I am currently being trained under the IT Operations Pratice to support business-critical responsibilities and specialist areas such as infrastructure and security. I will be trained and placed with a client contributing to enhancing key system reliability through integration and deployment. These efforts will lead to long-term operational success for the client. I will be able to take on roles like DevOps, System Admin, or Site Reliability Engineer.",
+    desc: "I am currently being trained under the IT Operations Pratice to support business-critical responsibilities and specialist areas such as infrastructure and security. I will be trained and placed with a client contributing to enhancing key system reliability through integration and deployment, leading to long-term operational success. I will be prepared to take on roles such as DevOps, System Admin, or Site Reliability Engineer.",
     link: "https://www.fdmgroup.com/us/",
     pos: "c",
   };
 
   return (
-    <div>
+    <div className="mb-36">
       <div className="text-white text-4xl md:text-6xl text-center pb-10 font-ss3 font-black">
         Relevant Work
       </div>
