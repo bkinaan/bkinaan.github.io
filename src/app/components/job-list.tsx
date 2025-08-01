a2e3d7d791a4e3a1b73e86cfc610dc436f6eaa49
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2025-08-01 16:49:31 -0400

    added new job at verition

--- src/app/components/job-list.tsx
+++ src/app/components/job-list.tsx
@@ -4,10 +4,19 @@ export default function Jobs() {
   let fdm = {
     role: "IT Operations Consultant",
     company: "FDM Group",
-    time: "Jun. 2025 - current",
-    desc: "I am currently being trained under the IT Operations Pratice to support business-critical responsibilities and specialist areas such as infrastructure and security. I will be trained and placed with a client contributing to enhancing key system reliability through integration and deployment, leading to long-term operational success. I will be prepared to take on roles such as DevOps, System Admin, or Site Reliability Engineer.",
+    time: "Jun. 2025 - Jul. 2025",
+    desc: "Under the IT Operations Pratice I trained to support business-critical responsibilities and specialist areas such as infrastructure and security. I worked to be placed with a client contributing to enhancing key system reliability through integration and deployment, leading to long-term operational success. Through this training I learned how to be prepared to take on roles such as DevOps, System Admin, Production Support Engineer or Site Reliability Engineer.",
     link: "https://www.fdmgroup.com/us/",
-    pos: "c",
+    pos: "r",
+  };
+
+  let ver = {
+    role: "Production Support Engineer",
+    company: "Verition Fund Management",
+    time: "Jul. 2025 - current",
+    desc: "As a Level 1 Production Support Engineer at Verition I monitor and support internal production systems to ensure high availablity, reliability, and performance for traders. Alongside a global team, I proactively identify, troubleshoot, and escalate technical issues, minimizing downtime and mitigating operational risk. I collaborate with trading, development, and infrastructure teams to drive quick resolutions and maintain system integrity. Through effective communication and coordination, I help ensure critical trading operations run smoothly in a fast-paced, high pressure environment.",
+    link: "https://verition.com",
+    pos: "l",
   };
 
   return (
@@ -16,6 +25,14 @@ export default function Jobs() {
         Relevant Work
       </div>
       <div className="grid grid-cols-2 gap-7">
+        <Job
+          role={ver.role}
+          company={ver.company}
+          time={ver.time}
+          desc={ver.desc}
+          link={ver.link}
+          pos={ver.pos}
+        ></Job>
         <Job
           role={fdm.role}
           company={fdm.company}
